05a7cdd8c651db66765331a2ff9a8ca91572fad9
yangyongqiang <779176691@qq.com>
2020-11-08 16:00:34 +0800

    分支添加

--- HelloGit/src/main/java/com/chunyouvip/git/HelloGit.java
+++ HelloGit/src/main/java/com/chunyouvip/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.chunyouvip.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("创建分支1");
+        System.out.println("dev分支");
     }
 }
