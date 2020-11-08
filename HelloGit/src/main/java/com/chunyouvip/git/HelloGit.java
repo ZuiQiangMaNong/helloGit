f7a55a0330f3281587ee46e7e9253935f79f929f
yangyongqiang <779176691@qq.com>
2020-11-08 15:59:29 +0800

    主干分支

--- HelloGit/src/main/java/com/chunyouvip/git/HelloGit.java
+++ HelloGit/src/main/java/com/chunyouvip/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.chunyouvip.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("创建分支1");
+        System.out.println("主干分支");
     }
 }
